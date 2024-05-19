088eac6b8bddbf3e2f6c7863b43b3b83827016c6
Allezan <95235539+Allezan@users.noreply.github.com>
2024-05-20 00:29:03 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+AMOUNT_THREAD = 1
+AMOUNT_LOOPS = 10
+AMOUNT_COOKIES = 1
+
+local = {
+    "executablePath": r"/chromedriver",
+    "userDataDir": r"C:/Users/Infinix/OneDrive/Dokumen/tiktok_auto_upload/Profiles/profile ",
+}
+
+DataImg = {
+    "imgFormat": r".mp4",
+    "imgDataDir": r"C:/Users/Infinix/OneDrive/Dokumen/tiktok_auto_upload/Videos",
+    "imgName": r"/VD ",
+}
